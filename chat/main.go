11bfe9e0a1d125e1b3e1e2fead16b39cf070fe7a
brownbro <hironao.namba@gmail.com>
2016-03-30 16:02:38 +0900

    Finish chapter 2

--- chat/main.go
+++ chat/main.go
@@ -7,6 +7,9 @@ import (
 	"path/filepath"
 	"sync"
 	"flag"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -19,16 +22,30 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 
+	gomniauth.SetSecurityKey("securitykeyhogehogehogehoge")
+	gomniauth.WithProviders(
+		google.New("158838570690-o1hndl14h6me61r7u0b3mepvk831gfeb.apps.googleusercontent.com", "Mly1zT1HSWNDkBOUZE1u9-Cn", "http://localhost:8080/auth/callback/google"),
+	)
+
 	r := newRoom()
 	//r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	go r.run()
